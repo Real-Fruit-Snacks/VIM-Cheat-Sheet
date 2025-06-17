a726f2470f02e278c7ffe4d9e3a6240a75ec8bbb
Matt <github.spindle@mattbwest.com>
2025-06-17 17:11:45 -0400

    chore: cleanup test files and screenshots from VIM project

--- test-nbsp.cjs
+++
@@ -1,9 +0,0 @@
-// Test non-breaking space
-const nbsp = '\u00A0';
-const regularSpace = ' ';
-
-console.log('Regular space code:', regularSpace.charCodeAt(0));
-console.log('NBSP code:', nbsp.charCodeAt(0));
-console.log('Test string:', `a${nbsp}b`);
-console.log('Includes regular space:', `a${nbsp}b`.includes(' '));
-console.log('Includes nbsp:', `a${nbsp}b`.includes(nbsp));
